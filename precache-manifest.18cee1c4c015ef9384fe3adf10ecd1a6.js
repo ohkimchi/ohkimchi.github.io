4ec40f280ec3992fd80449c438daf5efa2fb2ffc
Xu Junhui <maxjunhuixu@gmail.com>
2020-01-21 18:08:02 +0800

    test

---
+++ precache-manifest.18cee1c4c015ef9384fe3adf10ecd1a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f335b2764ff76a0a09f05b0a0c07b212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a047e0ffd62d6670ba",
+    "url": "/static/css/main.ae80000b.chunk.css"
+  },
+  {
+    "revision": "579ccf526771967368ba",
+    "url": "/static/js/2.53d90ab5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.53d90ab5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68a047e0ffd62d6670ba",
+    "url": "/static/js/main.6dab103e.chunk.js"
+  },
+  {
+    "revision": "717122a99b31db51b03f",
+    "url": "/static/js/runtime-main.88563e37.js"
+  }
+]);
